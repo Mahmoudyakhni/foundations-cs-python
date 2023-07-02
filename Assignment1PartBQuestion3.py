4e8bce98ebe791d0923a1059b228b47f638c1365
mahmoudyakhni <mahmoudyakhni1988@gmail.com>
2023-07-02 22:01:13 +0300

    assignment-ex3

---
+++ Assignment1PartBQuestion3.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  2 14:44:24 2023
+
+@author: mahmoud
+"""
+
+number = input("Enter a number to show how much digits contains: ")
+print(number,"has",len(number),"digits")
